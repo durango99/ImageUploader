fabc332adf434cd612069b115ff5c9452afbb569
durango99 <durango99@gmail.com>
2015-06-29 19:14:20 -0500

    initial push

---
+++ ImageUploader/capture.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="capture.aspx.cs" Inherits="ImageUploader.ImageUploader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Image Upload - Results</title>
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+
+    <h1>Media Capture Results</h1>
+
+    <div id="divContent" runat="server">
+    </div>
+
+
+</body>
+</html>
---
+++ ImageUploader/capture.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ImageUploader
+{
+    public partial class ImageUploader: System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpFileCollection uploadFiles = Request.Files;
+
+            string summary = "<p>number of files to upload:" + (uploadFiles.Count - 1) + "</p>";
+
+            // Build HTML listing the files received.
+            summary += "<p>Files Uploaded:</p><ol>";
+            
+            // Loop over the uploaded files and save to disk.
+            int i;
+            for (i = 0; i < uploadFiles.Count; i++)
+            {
+                HttpPostedFile postedFile = uploadFiles[i];
+
+                string pfilename = Path.GetFileName(postedFile.FileName);
+
+                if (pfilename.Length > 0)
+                {
+                    // Access the uploaded file's content in-memory:
+                    //System.IO.Stream inStream = postedFile.InputStream;
+                    //byte[] fileData = new byte[postedFile.ContentLength];
+                    //inStream.Read(fileData, 0, postedFile.ContentLength);
+
+                    // Save the posted file in our "data" virtual directory.
+                    //postedFile.SaveAs(Server.MapPath("images") + "\\" + postedFile.FileName);
+                    postedFile.SaveAs(Server.MapPath("~/images/") + pfilename);
+                
+                    //string fileName = postedFile.FileName;
+                    //fileName = Server.MapPath("~/images/" + fileName);
+                    //postedFile.SaveAs(fileName);
+
+                    // Also, get the file size and filename (as specified in
+                    // the HTML form) for each file:
+                    summary += "<li>" + pfilename + ": "
+                        + postedFile.ContentLength.ToString() + " bytes</li>";
+                }
+            }
+            summary += "</ol>";
+
+            // If there are any form variables, get them here:
+            //summary += "<p>Form Variables:</p><ol>";
+
+            //Load Form variables into NameValueCollection variable.
+            //NameValueCollection coll = Request.Form;
+
+            // Get names of all forms into a string array.
+            //String[] arr1 = coll.AllKeys;
+            //for (i = 0; i < arr1.Length; i++)
+            //{
+            //    summary += "<li>" + arr1[i] + "</li>";
+            //}
+            //summary += "</ol>";
+
+            divContent.InnerHtml = summary;
+
+
+        }
+    }
+}
